7d6f1578ebd87696c40957a5c17381b9e6fe597a
Austin Kong <austin.kong@unsw.edu.au>
2015-12-14 10:37:59 +1100

    Day 13

---
+++ 13/part2.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use List::Util qw/shuffle/;
+
+$debug = 0;
+
+while (<STDIN>) {
+    /(\w+) would (\w+) (\d+) happiness units by sitting next to (\w+)./ || next;
+    $happiness{$1}{$4} = $3;
+    $happiness{$1}{$4} *= -1 if ($2 eq "lose");
+}
+
+foreach $name1 (sort keys %happiness) {
+	$happiness{"me"}{$name} = 0;
+	$happiness{$name}{"me"} = 0;
+}
+
+$i = 0;
+%tested = ();
+$maxHappy = 0;
+while ($i < 50000) {
+	$i++;
+	@arr = shuffle(keys %happiness);
+	$seatingPlan = join ' ', @arr;
+	next if exists $tested{$seatingPlan};
+	$tested{$seatingPlan} = 1;
+	$happy = 0;
+	$happy += $happiness{$arr[0]}{$arr[1]};
+	$happy += $happiness{$arr[0]}{$arr[$#arr]};
+	foreach $j (1..$#arr-1) {
+		$happy += $happiness{$arr[$j]}{$arr[$j+1]};
+		$happy += $happiness{$arr[$j]}{$arr[$j-1]};
+	}
+	$happy += $happiness{$arr[$#arr]}{$arr[$#arr-1]};
+	$happy += $happiness{$arr[$#arr]}{$arr[0]};
+	print "$seatingPlan: $happy\n" if $debug;
+	$maxHappy = $happy if $happy >= $maxHappy;
+}
+
+print "Max happiness: $maxHappy\n";
